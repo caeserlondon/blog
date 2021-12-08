37bfa686671eb11ba81d47e037dc2ae1f352c567
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2021-12-08 16:49:31 +0000

    updating the articles controler

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,31 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
-  def index; end
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
