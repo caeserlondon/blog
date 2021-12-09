c5551f73ea033a1e8ed45e27e11d554095b99105
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2021-12-09 06:02:42 +0000

    adding concerns

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  include Visible
   has_many :comments
 
   validates :title, presence: true
---
+++ db/migrate/20211209053117_add_status_to_article.rb
@@ -0,0 +1,5 @@
+class AddStatusToArticle < ActiveRecord::Migration[6.1]
+  def change
+    add_column :articles, :status, :string
+  end
+end
